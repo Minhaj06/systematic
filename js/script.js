a6fd0ae15bd76f63a09b435e9b16b012e5472f43
Minhaj06 <84719299+Minhaj06@users.noreply.github.com>
2021-06-18 21:29:47 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Bx Slider
+jQuery(document).ready(function(){
+    jQuery('.slider_area').bxSlider();
+    jQuery('.inner_menu').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth: 1025,
+    });
+  });
